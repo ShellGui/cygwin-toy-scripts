bd294503a724cba8ddee489da7e81c740a67fe95
ekee <417@xmlad.com>
2017-01-11 05:25:14 +0800

    Wed Jan 11 05:25:14 CST 2017

--- get_gtk364.sh
+++ get_gtk364.sh
@@ -7,6 +7,9 @@ Work_Root=$(pwd)
 if [ ! -f $Work_Root/srcs/gtk364/gtk+-bundle_3.6.4-20130513_win32.zip ] && [ "$(md5sum $Work_Root/srcs/gtk364/gtk+-bundle_3.6.4-20130513_win32.zip | cut -d ' ' -f1)" = "e47c3c10a2d8c5267c210d875a9d5341" ]; then
 mkdir -p $Work_Root/srcs/gtk364/
 echo wget http://www.tarnyko.net/repo/gtk3_build_system/gtk+-bundle_3.6.4-20130513_win32.zip -P $Work_Root/srcs/gtk364/
+fi
+
+if [ ! -d $Work_Root/srcs/gtk364/gtk+-bundle_3.6.4-20130513_win32 ]; then
 cd $Work_Root/srcs/gtk364/
-unzip test.zip -d gtk+-bundle_3.6.4-20130513_win32
+unzip gtk+-bundle_3.6.4-20130513_win32.zip -d gtk+-bundle_3.6.4-20130513_win32
 fi
